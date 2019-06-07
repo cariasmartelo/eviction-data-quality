4ae7e236e44c258aac389ab02e480e8973d28b2b
Camilo Arias <cariasmartelo@uchicago.edu>
2019-06-07 08:52:07 -0500

    Results

--- scripts/descriptives.py
+++ scripts/descriptives.py
@@ -6,7 +6,48 @@ import geopandas as gpd
 
 
 
-def plot_map(eviction_gdf, variable, years, change = False):
+def plot_map(eviction_gdf, variable, years):
+    '''
+    Map by zip code the value of the column indicated in colorcol and the year.
+    Inputs:
+        eviction_gdf: GeoDataFrame
+        variable: Str
+        year: int or list
+    Output:
+        Map
+    '''
+    if isinstance(years, int):
+        year = years
+        fig, ax = plt.subplots(figsize=(8, 12))
+        eviction_gdf.plot(color="grey", ax=ax, edgecolor="black")
+        eviction_gdf.loc[(eviction_gdf['year'] == year) &\
+                        (eviction_gdf[variable].notna())].plot(ax=ax, 
+                                                               column=variable,
+                                                               cmap='viridis',
+                                                               scheme='quantiles',
+                                                               legend=True)
+
+        ax.set_title('Tracts of Chicago by {} in {}\n(Tracts without data'
+                     ' in grey)'.format(" ".join(variable.split("_")),
+                                        year))
+    else:
+        fig, ax = plt.subplots(nrows = len(years), figsize=(8, 12))
+        for i, year in enumerate(years):
+            eviction_gdf.plot(color="grey", ax=ax[i], edgecolor="black")
+            eviction_gdf.loc[(eviction_gdf['year'] == year) &\
+                            (eviction_gdf[variable].notna())].plot(ax=ax[i], 
+                                                                   column=variable,
+                                                                   cmap='viridis',
+                                                                   scheme='quantiles',
+                                                                   legend=True)
+
+            ax[i].set_title('Tracts of Chicago by {} in {}\n(Tracts without data'
+                         ' in grey)'.format(" ".join(variable.split("_")),
+                                            year))
+    plt.show()
+
+
+def plot_map_change(eviction_gdf, variable, years):
     '''
     Map by zip code the value of the column indicated in colorcol and the year.
     Inputs:
